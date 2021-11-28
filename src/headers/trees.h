2d2ab68aece7b7c6197ded4bfab010fc0c11c23e
Andrea Berardi <Kotheos@protonmail.com>
2021-11-28 18:22:49 +0100

    added new header for future use

---
+++ src/headers/trees.h
@@ -0,0 +1,9 @@
+// Header guards:
+// patterns of preprocessor directives that protect a header from being included multiple times
+#ifndef TREES_TREES_H
+#define TREES_TREES_H
+
+#endif //TREES_TREES_H
+
+// #pragma once
+// an alternative to header guards
