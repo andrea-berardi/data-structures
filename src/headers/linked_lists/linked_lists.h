5aadf690e6e7001b685d77893f4988557767062b
Andrea Berardi <Kotheos@protonmail.com>
2021-11-28 19:28:45 +0100

    implementing linked lists that are degenerated trees

--- src/headers/linked_lists/linked_lists.h
+++ src/headers/linked_lists/linked_lists.h
@@ -10,7 +10,7 @@ typedef struct ll_node {
 } LLNode;
 
 typedef struct ll_head {
-    size_t cardinality; // practical usage: it'll define the dimension of the arrays we'll use for "TreeWalks"
+    size_t cardinality; // amount of elements in the linked list
     struct ll_node* head;
 } LL;
 
@@ -19,6 +19,8 @@ LLNode *LLListSearch(LL *L, int k);
 void LLListInsert(LL *L, LLNode *x);
 
 void LLListDelete(LL *L, LLNode *x);
+
+void LLNewList(LLNode *x);
 
 #endif //TREES_LINKED_LISTS_H
 
