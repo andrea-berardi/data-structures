ad120cc694fec1559c4b8ec007d7f0487e2277ad
Andrea Berardi <Kotheos@protonmail.com>
2021-12-02 22:03:16 +0100

    red-black trees' procs

--- src/headers/data_structures/red_black_trees.h
+++ src/headers/data_structures/red_black_trees.h
@@ -3,6 +3,28 @@
 #ifndef TREES_RED_BLACK_TREES_H
 #define TREES_RED_BLACK_TREES_H
 
+#include <stdlib.h>
+
+// the nodes will either be "red" or "black"
+typedef enum color {
+    RED = 0,
+    BLACK = 1,
+} Color;
+
+typedef struct rbt_node {
+    int key;
+    Color color;
+    struct rbt_node *parent;
+    struct rbt_node *left;
+    struct rbt_node *right;
+} RBTNode;
+
+typedef struct rbt_tree {
+    size_t cardinality; // practical usage: it'll define the dimension of the arrays we'll use for "TreeWalks"
+    struct rbt_node *nil;  // sentinel node `T.nil`
+    struct rbt_node *root;
+} RBTTree;
+
 #endif //TREES_RED_BLACK_TREES_H
 
 // #pragma once
--- src/modules/data_structures/red_black_trees.c
+++ src/modules/data_structures/red_black_trees.c
@@ -1,1 +1,74 @@
 #include "../../headers/data_structures/red_black_trees.h"
+
+void RBTLeftRotate(RBTTree *T, RBTNode *x) {
+    RBTNode *y = x->right;
+    x->right = y->left;
+
+    if (y->left != T->nil) {
+        y->left->parent = x;
+    }
+
+    y->parent = x->parent;
+
+    if (x->parent == T->nil) {
+        T->root = y;
+    } else if (x == x->parent->left) {
+        y->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+
+    y->left = x;
+    x->parent = y;
+}
+
+void RBTRightRotate(RBTTree *T, RBTNode *x) {
+    RBTNode *y = x->left;
+    x->left = y->right;
+
+    if (y->right != T->nil) {
+        y->right->parent = x;
+    }
+
+    y->parent = x->parent;
+
+    if (x->parent == T->nil) {
+        T->root = y;
+    } else if (x == x->parent->right) {
+        y->parent->right = y;
+    } else {
+        x->parent->left = y;
+    }
+
+    y->right = x;
+    x->parent = y;
+}
+
+void RBTInsert(RBTTree *T, RBTNode *z) {
+    RBTNode *y = T->nil;
+    RBTNode *x = T->root;
+
+    while (x != T->nil) {
+        y = x;
+
+        if (z->key < x->key) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+
+    if (y == T->nil) {
+        T->root = z;
+    } else if (z->key < y->key) {
+        y->left = z;
+    } else {
+        y->right = z;
+    }
+
+    z->left = T->nil;
+    z->right = T->nil;
+    z->color = RED;
+
+    // RBTTreeInsertFixup()
+}
