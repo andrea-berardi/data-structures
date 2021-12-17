cb1cd362c9fe43d19d1e951df36458f78cb5af70
Andrea Berardi <Kotheos@protonmail.com>
2021-12-17 23:57:06 +0100

    B-Trees are coming MUAHAHAHAH

---
+++ src/headers/data_structures/b_trees.h
@@ -0,0 +1,9 @@
+// Header guards:
+// patterns of preprocessor directives that protect a header from being included multiple times
+#ifndef DATA_STRUCTURES_B_TREES_H
+#define DATA_STRUCTURES_B_TREES_H
+
+#endif //DATA_STRUCTURES_B_TREES_H
+
+// #pragma once
+// an alternative to header guards
