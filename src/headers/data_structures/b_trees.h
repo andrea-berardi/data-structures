920883f7d6a3ff0865a78bc645407459c9070e39
Andrea Berardi <Kotheos@protonmail.com>
2021-12-21 00:39:46 +0100

    implementing btrees

--- src/headers/data_structures/b_trees.h
+++ src/headers/data_structures/b_trees.h
@@ -4,8 +4,22 @@
 #define DATA_STRUCTURES_B_TREES_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
+#define t 1000 // maximum numbers of keys that a node can have - it has to be >= 2
 
+typedef struct bt_node {
+    size_t n; // cardinality of the node
+    int keys[2 * t]; // keys actually stored in the node (NOT in descending order)
+    bool leaf; // `true` if the node is a leaf, `false` if it's an internal node
+    struct bt_node *parent;
+    struct bt_node *children[2 * t + 1]; // pointers to the children
+} BTNode;
+
+typedef struct bt_tree {
+    size_t cardinality; // `x.n`
+    struct bt_node *root;
+} BTTree;
 
 #endif //DATA_STRUCTURES_B_TREES_H
 
