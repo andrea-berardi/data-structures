db6ba1a40cf4b3ac5267211e54b44b23f33fbeea
Andrea Berardi <Kotheos@protonmail.com>
2022-02-27 01:46:50 +0100

    suppress warning

--- src/modules/experiments/lab_3.c
+++ src/modules/experiments/lab_3.c
@@ -69,6 +69,150 @@ exp_BT(ssize_t t, size_t max_keys, size_t max_search, size_t max_delete, size_t 
     return (long double) t_tot / (long double) max_instances;
 }
 
+long double exp_BST_1(size_t max_keys, size_t max_search, size_t max_delete, size_t max_instances, const bool DEBUG) {
+    clock_t t_tot = 0;
+
+    for (size_t instance = 1; instance <= max_instances; ++instance) {
+        clock_t t_start, t_end;
+
+        // I'm creating these arrays of random integer values for many reasons:
+        // - I can keep track of what I'm generating
+        // - I won't be adding any overheads by using the modulus operator (%) during the experiments
+        // - By using "small" values I'll be able to tell if weird things are happening to my arrays
+        // - I'll be able to compare the generated arrays and the generated BSTs more easily
+        // Also, I'm using different arrays in order not to create outliers.
+        int *rand_insert_values_pool = gen_rnd_array(max_keys);
+        int *rand_search_values_pool = gen_rnd_array(max_search);
+        int *rand_delete_values_pool = gen_rnd_array(max_delete);
+
+        // I'll use these indexes to scan the arrays. They'll be incremented at each run.
+        size_t insert_index = 0;
+        size_t search_index = 0;
+        size_t delete_index = 0;
+
+        // I'm declaring these here so that I can run debug checks on them before returning
+        BSTTree *T = BSTNewTree(NULL);
+
+        t_start = clock();
+        for (size_t key = 0; key < max_keys; ++key) {
+            // it's important to use `i++` instead of `++i`
+            BSTInsertKey(T, rand_insert_values_pool[insert_index++]);
+        }
+
+        for (size_t key = 0; key < max_search; ++key) {
+            // it's important to use `i++` instead of `++i`
+            BSTIterativeSearch(T->root, rand_search_values_pool[search_index++]);
+        }
+
+        for (size_t key = 0; key < max_delete; ++key) {
+            // This uses the iterative version of BSTSearch, but both versions are implemented and functional.
+            // I can't see any performance differences between the two, but the iterative version should be the go-to choice.
+            BSTDeleteKey(T,
+                         rand_delete_values_pool[delete_index++]); // it's important to use `i++` instead of `++i`
+        }
+        t_end = clock();
+
+        t_tot += t_end - t_start;
+
+        if (DEBUG) {
+            int *array = new_array(T->cardinality);
+            size_t index = 0;
+            BSTInOrderTreeWalkToArray(T->root, array, &index);
+
+            if (is_sorted(array, T->cardinality))
+                printf("Array sorted successfully (length: %zu)\n", T->cardinality);
+            else
+                fprintf(stderr, "The array was not sorted correctly.\n"), print_array(array, T->cardinality);
+
+            FreeNull_Int(array);
+        }
+
+        /* The following lines will give back to the OS the dynamic memory previously allocated */
+
+        // Destroying the data structures of the test
+        BSTDestroyTree(T); // destroying the tree and all its leaves (nodes) - `T` is now NULL
+
+        // Destroying the arrays
+        FreeNull_Int(rand_insert_values_pool);
+        FreeNull_Int(rand_search_values_pool);
+        FreeNull_Int(rand_delete_values_pool);
+    }
+
+    return (long double) t_tot / (long double) max_instances;
+}
+
+long double exp_RBT_1(size_t max_keys, size_t max_search, size_t max_delete, size_t max_instances, const bool DEBUG) {
+    clock_t t_tot = 0;
+
+    for (size_t instance = 1; instance <= max_instances; ++instance) {
+        clock_t t_start, t_end;
+
+        // I'm creating these arrays of random integer values for many reasons:
+        // - I can keep track of what I'm generating
+        // - I won't be adding any overheads by using the modulus operator (%) during the experiments
+        // - By using "small" values I'll be able to tell if weird things are happening to my arrays
+        // - I'll be able to compare the generated arrays and the generated BSTs more easily
+        // Also, I'm using different arrays in order not to create outliers.
+        int *rand_insert_values_pool = gen_rnd_array(max_keys);
+        int *rand_search_values_pool = gen_rnd_array(max_search);
+        int *rand_delete_values_pool = gen_rnd_array(max_delete);
+
+        // I'll use these indexes to scan the arrays. They'll be incremented at each run.
+        size_t insert_index = 0;
+        size_t search_index = 0;
+        size_t delete_index = 0;
+
+        // I'm declaring these here so that I can run debug checks on them before returning
+        RBTTree *T = RBTNewTree(NULL);
+
+        t_start = clock();
+        for (size_t key = 0; key < max_keys; ++key) {
+            // it's important to use `i++` instead of `++i`
+            RBTInsertKey(T, rand_insert_values_pool[insert_index++]);
+        }
+
+        for (size_t key = 0; key < max_search; ++key) {
+            // it's important to use `i++` instead of `++i`
+            RBTIterativeSearch(T, T->root, rand_search_values_pool[search_index++]);
+        }
+
+        for (size_t key = 0; key < max_delete; ++key) {
+            // This uses the iterative version of RBTSearch, but both versions are implemented and functional.
+            // I can't see any performance differences between the two, but the iterative version should be the go-to choice.
+            RBTDeleteKey(T, rand_delete_values_pool[delete_index++]); // it's important to use `i++` instead of `++i`
+        }
+        t_end = clock();
+
+        t_tot += t_end - t_start;
+
+        if (DEBUG) {
+            int *array = new_array(T->cardinality);
+            size_t index = 0;
+
+            RBTInOrderTreeWalkToArray(T, T->root, array, &index);
+
+            if (is_sorted(array, T->cardinality))
+                printf("Array sorted successfully (length: %zu)\n", T->cardinality);
+            else
+                fprintf(stderr, "The array was not sorted correctly.\n"), print_array(array, T->cardinality);
+
+            FreeNull_Int(array);
+        }
+
+        /* The following lines will give back to the OS the dynamic memory previously allocated */
+
+        // Destroying the data structures of the test
+        RBTDestroyTree(T); // destroying the tree and all its leaves (nodes) - `T` is now NULL
+
+        // Destroying the arrays
+        FreeNull_Int(rand_insert_values_pool);
+        FreeNull_Int(rand_search_values_pool);
+        FreeNull_Int(rand_delete_values_pool);
+    }
+
+    return (long double) t_tot / (long double) max_instances;
+}
+
 void lab_3(char file[], Configuration conf, bool DEBUG) {
     FILE *fp = fopen(file, "w+");
     if (fp == NULL) {
@@ -84,15 +228,15 @@ void lab_3(char file[], Configuration conf, bool DEBUG) {
         size_t max_delete = keys - max_search;
 
         srand(conf.seed);
-        //long double time_BST = exp_BST(keys, max_search, max_delete, conf.max_instances, DEBUG);
+        long double time_BST = exp_BST_1(keys, max_search, max_delete, conf.max_instances, DEBUG);
 
         srand(conf.seed);
-        //long double time_RBT = exp_RBT(keys, max_search, max_delete, conf.max_instances, DEBUG);
+        long double time_RBT = exp_RBT_1(keys, max_search, max_delete, conf.max_instances, DEBUG);
 
         srand(conf.seed);
         long double time_BT = exp_BT(conf.t, keys, max_search, max_delete, conf.max_instances, DEBUG);
 
-        fprintf(fp, "%zu,%Lf\n", keys, time_BT);
+        fprintf(fp, "%zu,%Lf,%Lf,%Lf\n", keys, time_BST, time_RBT, time_BT);
 
         ++conf.seed;
     }
--- src/plotter.py
+++ src/plotter.py
@@ -141,11 +141,54 @@ def lab_2() -> None:
     plt.clf() # clear the figure
 # end lab_2()
 
+# Lab. 3 - B-Trees, Red-Black Trees, Binary Search Trees
+def lab_3() -> None:
+    with open(dir + dir_lab_3 + ext, 'r') as file:
+        lines: List(str) = [line.strip() for line in file.readlines()]
+
+        keys: List(int) = []
+        second_column: List(float) = []
+        thrid_column: List(float) = []
+        fourth_column: List(float) = []
+
+        titles: List(str) = []
+
+        for line in iter(lines):
+            # check if line has any letters
+            if not any(char.isalpha() for char in line):
+                line: List(str) = line.split(',')
+
+                keys.append(int(line[0]))
+                second_column.append(float(line[1]))
+                thrid_column.append(float(line[2]))
+                fourth_column.append(float(line[3]))
+            else:
+                line: List(str) = line.split(',')
+
+                titles.append(line[1]) # title of the second column
+                titles.append(line[2]) # title of the third column
+                titles.append(line[3]) # title of the fourth column
+            # end if
+        # end for
+
+        plt.xlabel(x_label); plt.ylabel(y_label) # set axis labels
+        plt.plot(keys, second_column, 'red', keys, thrid_column, 'blue', linewidth = 2) # plot the data
+        plt.title(title_lab_2) # set title
+        plt.grid(True) # show the grid
+        plt.legend(titles, loc = 'best') # show legend, and put it in the best location
+        plt.xlim(0); plt.ylim(0) # set the lower limits of the x and y axis
+        plt.savefig(dir + dir_lab_3, dpi = quality) # save the plot PNG
+    # end with
+
+    plt.clf() # clear the figure
+# end lab_3()
+
 # Main function
 def main() -> None:
     lab_1A()
     lab_1B()
     lab_2()
+    lab_3()
 # end main()
 
 # this specifies what will be run when the script is executed
