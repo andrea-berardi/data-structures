8e61ef3c064268aeff72612653371fef677aeb9c
Andrea Berardi <69171871+andrea-berardi@users.noreply.github.com>
2022-03-17 11:00:28 +0100

    added docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['diskread_0',['DiskRead',['../b__trees_8c.html#a80de1418255dfbaf4932932337373523',1,'b_trees.c']]],
-  ['diskwrite_1',['DiskWrite',['../b__trees_8c.html#aa60daeac9e864e69df509d6b422ae7bd',1,'b_trees.c']]]
+  ['data_20structures_0',['Data Structures',['../index.html',1,'']]],
+  ['diskread_1',['DiskRead',['../b__trees_8c.html#a80de1418255dfbaf4932932337373523',1,'b_trees.c']]],
+  ['diskwrite_2',['DiskWrite',['../b__trees_8c.html#aa60daeac9e864e69df509d6b422ae7bd',1,'b_trees.c']]]
 ];
